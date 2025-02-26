ff9fa054e4563992a1580e4c01bf1137d69edce5
Rjpererag <rjperera98@gmail.com>
2025-02-26 13:43:46 +0100

    fix/status code validation and parsing data

--- rapidapi/endpoints/yahoo_finance.py
+++ rapidapi/endpoints/yahoo_finance.py
@@ -1,4 +1,5 @@
 from ..requests.requestor import EndpointRequestor
+from ..dataclasses import *
 
 
 class YahooFinanceAPI:
@@ -7,11 +8,20 @@ class YahooFinanceAPI:
         self.headers = headers
         self.requestor = EndpointRequestor()
 
-    def get_stock_quotes(self, ticker: str):
+    def get_stock_quotes(self, ticker: str) -> StockQuotes:
         url = "https://yahoo-finance15.p.rapidapi.com/api/v1/markets/stock/quotes"
         params = {
             "ticker": ticker
         }
-        return self.requestor.get(
+
+        response = self.requestor.get(
             url=url, headers=self.headers, params=params
         )
+
+        status = response.get("meta", {}).get("status")
+        if status == 200:
+            return StockQuotes(
+                status=status,
+                response=[QuoteBodyModel(**item) for item in response.get("body", [])]
+            )
+
