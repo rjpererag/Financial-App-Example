f866e2c13396f76144523922c70ca41cde3ab045
Rjpererag <rjperera98@gmail.com>
2025-03-11 16:53:37 +0100

    main files to consume/produce

--- main_consume.py
+++ main_consume.py
@@ -1,37 +1,11 @@
-from message_broker.consumers.rapidapi_consumers import receive_message
-from src.settings.message_broker import MessageBrokerSettings
-from src.service.market_db_handler import DBHandler
-from src.settings.db_authentication import DBCredentials
-
-from src.utils.logger import logger
-
-from decouple import config
-import json
-
-
-def decode_message(body: bytes) -> dict | None:  # TODO: Move to utils
-
-    try:
-        mssg = body.decode("utf-8")
-        return json.loads(mssg)
-    except Exception as e:
-        logger.error(f"Error decoding message: {str(body)}. {str(e)}")
-
-
-def call_back_func(body):
-    if message := decode_message(body=body):
-        logger.info(f"Loading data into db")
-        return message
+from src.message_broker.consumers.rapidapi_consumers import receive_message
+from src.message_broker.callbacks.market_value_db import call_back_func
+from src.settings import create_settings
 
 
 if __name__ == "__main__":
-    settings = MessageBrokerSettings()
-    db_settings = DBCredentials(
-        db_user=config("DB_USER"),
-        db_password=config("DB_PASSWORD"),
-        db_name=config("DB_NAME"),
-        db_port=config("DB_PORT"),
-    )
-
-    settings.callback_func = call_back_func
-    receive_message(settings=settings, db_settings=db_settings)
+    settings = create_settings()
+    settings.broker.callback_func = call_back_func
+    receive_message(
+        broker_settings=settings.broker,
+        db_settings=settings.db)
--- main_sender.py
+++ main_sender.py
@@ -6,5 +6,4 @@ if __name__ == "__main__":
     settings = MessageBrokerSettings()
     path = "test_data/fake_rapidapi_data/data.pkl"
     api_consumer = FinanceAPIConsumer(mssg_broker_settings=settings)
-
     api_consumer.local_api_consumption(data_path=path)
