3b4f894731f73432a65f9a9d8bfd0391c5476cd2
Rjpererag <rjperera98@gmail.com>
2025-03-11 16:52:43 +0100

    fix/using relative imports

--- src/service/finance_api.py
+++ src/service/finance_api.py
@@ -1,11 +1,13 @@
 import json
 from datetime import datetime
-from src.utils.file_manager import FileManager
-from src.utils.logger import logger
-from message_broker.producers.rapidapi_producer import send_message
-from src.settings.message_broker import MessageBrokerSettings
+
 from rapidapi.dataclasses.stock_quotes import StockQuotes
 from rapidapi.dataclasses.body.stock_quotes import QuoteBodyModel
+
+from ..utils.file_manager import FileManager
+from ..utils.logger import logger
+from ..message_broker.producers.rapidapi_producer import send_message
+from ..settings.message_broker import MessageBrokerSettings
 
 from time import sleep
 
@@ -22,7 +24,7 @@ class FinanceAPIConsumer:
         now_str = now.strftime(f"%Y-%m-%H-%M-%S")
         return {"data": {**response.model_dump()}, "action": action, "time": now_str}
 
-    def _create_message(self, response: list[QuoteBodyModel]) -> str | None:
+    def _create_message(self, response: list[QuoteBodyModel]) -> bytes:
         msg = {resp.symbol: self._format_response(response=resp, action="insert")
                for resp in response if resp is not None} if isinstance(response, list) else None
 
@@ -39,11 +41,10 @@ class FinanceAPIConsumer:
                     send_message(
                         settings=self.mssg_broker_settings,
                         message=msg)
-                sleep(5)  # TODO: Delete
+                sleep(5)
 
             except Exception as e:
                 logger.error(f"Error occurred: {str(e)}. Data: {call}")
-
 
     def consume_api(self):
         # TODO
